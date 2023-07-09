f79605de51be1833143cede2fe9691ea3a30ef37
Mckay Jensen <jensenm@uchicago.edu>
2023-07-09 17:22:12 -0500

    Updates

---
+++ _app/immutable/chunks/navigation.baf70231.js
@@ -0,0 +1,1 @@
+import{h as o}from"./singletons.35d20eed.js";const e=o("goto");export{e as g};
---
+++ _app/immutable/chunks/stores.067376b0.js
@@ -0,0 +1,1 @@
+import{w as s}from"./index.30275463.js";const t=s([]),l=s(null);export{t as a,l as u};
